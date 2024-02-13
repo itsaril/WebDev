1eebab6b1e8df6483d125fa44eecffa46192c351
Arailym Myrzakhankyzy <123051136+itsaril@users.noreply.github.com>
2024-02-14 00:44:01 +0600

    Add files via upload

---
+++ Lab 3/tasks/42.js
@@ -0,0 +1,8 @@
+function checkAge(age) {
+    return (age > 18) ? true : confirm('Did parents allow you?');
+  }
+//   Using OR (the shortest variant):||
+  
+  function checkAge(age) {
+    return (age > 18) || confirm('Did parents allow you?');
+  }
---
+++ Lab 3/tasks/63.js
@@ -0,0 +1,4 @@
+alert( 6.35.toFixed(20) );
+alert( 1.35.toFixed(20) );
+alert( (6.35 * 10).toFixed(20) );
+alert( Math.round(6.35 * 10) / 10 );
