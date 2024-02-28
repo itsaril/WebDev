05cb3b0c2d93e02d5707892d3eb7298c45d250ad
Arailym Myrzakhankyzy <123051136+itsaril@users.noreply.github.com>
2024-02-28 20:16:56 +0600

    Add files via upload

---
+++ Lab 5/src/app/category.service.ts
@@ -0,0 +1,27 @@
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export const categories = [
+  {
+    id: 1,
+    name: 'сумка багет',
+  },
+  {
+    id: 2,
+    name: 'сумка кросс-боди',
+  },
+  {
+    id: 3,
+    name: 'сумка тоут',
+  },
+  {
+    id: 4,
+    name: 'клатч ',
+  },
+  {
+    id: 5,
+    name:'сумка торба',
+  }
+];
